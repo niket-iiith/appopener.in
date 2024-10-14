8ef1c281acd0c46d8bec9b4b08b29909f9d31632
prabhatsharma21 <prabhatsharmabest1@gmail.com>
2024-10-14 13:53:23 +0530

    test:ads

--- src/components/adsComponent.jsx
+++ src/components/adsComponent.jsx
@@ -11,7 +11,7 @@ const AdComponent = () => {
   }, []);
 
   return (
-    <div className="adsbyvli" data-ad-slot="pw_42234" style={{ margin: '20px 0', textAlign: 'center' }}>
+    <div className="adsbyvli" data-ad-slot="pw_42234" style={{ margin: '20px 0', textAlign: 'center', width: '400px', height:'500px'}}>
       {/* The ad will be displayed here */}
     </div>
   );
